0327571ced4ec1f80f444b4110ce77b6f37323a5
UnesABA <unesaitbenali@gmail.com>
2025-03-15 15:28:26 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode }     from 'react'
+import { createRoot }     from 'react-dom/client'
+import { Provider }       from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import './index.css'
+import App from './App.jsx'
+
+const store = configureStore({
+    reducer: {
+      
+    }
+})
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store= {store}>
+      <App />
+    </Provider>
+  </StrictMode>
+)
